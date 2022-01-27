f5c735f215e563486a484cd1bb38846294ce28c7
George Bekmezian <george.bekmezian@cvetech.com>
2022-01-27 16:58:24 -0700

    run delete method

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, json, request, Response, render_template, stream_with_context
+from weakref import ref
+from flask import Flask, json, session, request, Response, render_template, stream_with_context
 import json, sys, requests, re
 import os
 import datetime
@@ -30,6 +31,10 @@ def index():
             expiresIn = codeInfo[1]
             refreshToken = codeInfo[2]
             redirectUri = codeInfo[3]
+            session['accessToken'] = accessToken
+            session['expiresIn'] = expiresIn
+            session['refreshToken'] = refreshToken
+            session['redirectUri'] = redirectUri
             return render_template("removeUser.html.jinja", accessStuff = accessToken)
             
         return "Auth failed. Review logs"
@@ -42,12 +47,26 @@ def removeUserRender():
     cveLogger.mylogger(f'{cveLogger.lineno()} got here with request referrer: {request.referrer}')   
     return render_template("removeUser.html.jinja")
 
-@app.route("/removeUser", methods=["POST"])
-def removeUser():
+@app.route("/removeUserStatus", methods=["POST", "GET"])
+def removeUserStatus():
 
     # Webhook Receiver
     cveLogger.mylogger(f'{cveLogger.lineno()} got here with request: {request.args}')
-    return "Stil under development"
+    
+    if request.method == "POST":
+        cveLogger.mylogger(f'{cveLogger.lineno()} request method is post')
+        cveLogger.mylogger(f'{cveLogger.lineno()} Request content type: {request.content_type}')
+        if 'accessToken' in session:
+            accessToken = session['accessToken']
+            session['emailAddress'] = request.args.get('emailAddress')
+        else:
+            return index()
+
+        return render_template("removeUserStatus.html.jinja", accessStuff = accessToken)
+
+    cveLogger.mylogger(f'{cveLogger.lineno()} request method is not post') 
+    if 'emailAddress' in session:
+        return app.response_class(listUserMemberships.listUserMemberships(session['accessToken'], session['emailAddress'], ''), mimetype="text/plain")
 
 
 
