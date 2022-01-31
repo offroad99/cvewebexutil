ef399c20da64fba76abb09e86a3098884e158040
George Bekmezian <george.bekmezian@cvetech.com>
2022-01-31 14:12:01 -0700

    fix streaming updates

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, json, session, request, Response, render_template, stre
 import json, sys, requests, re
 import os
 import datetime
+from time import sleep
 import cveLogger
 import sys
 import getUserToken
@@ -73,16 +74,48 @@ def removeUserStatus():
         return render_template("removeUserStatus.html.jinja", accessStuff = accessToken)
 
     cveLogger.mylogger(f'{cveLogger.lineno()} request method is not post') 
+    
+    def myIter():
+        yield ' '*1024
+        for thething in listUserMemberships(session['accessToken'], session['emailAddress'], ''):
+            yield thething
+        return None
+
     if 'emailAddress' in session:
         cveLogger.mylogger(f'{cveLogger.lineno()} Calling listUserMemberships with email address: {session["emailAddress"]}') 
-        return app.response_class(stream_with_context(listUserMemberships(session['accessToken'], session['emailAddress'], '')), mimetype="text/plain")
+        return app.response_class(stream_with_context(myIter()), mimetype="text/plain")
     
     return index()
 
+@app.route('/streamtest')
+def streamtest():
+    return render_template("streamtesting.html.jinja")
 
+@app.route('/get_stream')
+def streamed_response():
+    def generate():
+        doagain = 0
+        yield ' '*1024
+        while doagain < 40:
+            print("yield 1 " + str(doagain))
+            yield str(doagain) + ' Hello \n'
+            sleep(3)
+            print("yield 2" + str(doagain))
+            yield str(doagain) + ' this is nothing \n'
+            sleep(3)
+            print("yield 3")
+            yield str(doagain) + ' bang!\n'
+            doagain = doagain + 1
+        return None
+    #return app.response_class(stream_with_context(generate()), mimetype="text/plain")
+    return app.response_class(generate(), mimetype='text/plain')
 
 if __name__ == "__main__":
     cveLogger.initlogging(sys.argv)
     cveLogger.mylogger(f'{cveLogger.lineno()} About to begin web server on port 5001')
-    app.run(host="0.0.0.0", port=443, debug=True, 
-        ssl_context=('/etc/letsencrypt/live/cveautomation.com/fullchain.pem', '/etc/letsencrypt/live/cveautomation.com/privkey.pem'))
+
+    if os.path.exists('/etc/letsencrypt/live/cveautomation.com/privkey.pem'):
+        app.run(host="0.0.0.0", port=443, debug=True, 
+            ssl_context=('/etc/letsencrypt/live/cveautomation.com/fullchain.pem', '/etc/letsencrypt/live/cveautomation.com/privkey.pem'))
+    else:
+        app.run(host="0.0.0.0", port=443, debug=True, ssl_context='adhoc')
